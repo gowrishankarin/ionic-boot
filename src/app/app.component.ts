29d54cb9124b88e009bc161bf7b75b1b69e8551e
Gowri Shankar <shankar.chari@gmail.com>
2017-06-10 23:37:24 +0530

    New component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = "Gary's App";
+
+    sayHellp() {
+        alert('Hello, World');
+    }
 }
